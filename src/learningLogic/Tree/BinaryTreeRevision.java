007ef0ea2a7e04c44e6a1f8cdd1332d1ba55fff2
Nikhil <Nikhil.Thulaseedharan@gmail.com>
2022-05-04 22:47:53 +0530

    Tree 1.inorder 2.preorder 3.postorder

---
+++ src/learningLogic/Tree/BinaryTreeRevision.java
@@ -0,0 +1,75 @@
+package learningLogic.Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTreeRevision {
+
+	public Node root;
+
+	class Node {
+		private Node left;
+		private Node right;
+		private int data;
+
+		Node(int data) {
+			this.data = data;
+		}
+	}
+
+	public void insert(int data) {
+		Node node = new Node(data);
+		if (root == null) {
+			root = node;
+			return;
+		}
+
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(root);
+		while (!queue.isEmpty()) {
+			Node temp = queue.peek();
+			queue.remove();
+
+			if (temp.left == null) {
+				temp.left = node;
+				return;
+			} else {
+				queue.add(temp.left);
+			}
+
+			if (temp.right == null) {
+				temp.right = node;
+				return;
+			} else {
+				queue.add(temp.right);
+			}
+		}
+
+	}
+
+	public void inOrderTraversal(Node node) {
+		if (node != null) {
+			inOrderTraversal(node.left);
+			System.out.print(node.data + "-->");
+			inOrderTraversal(node.right);
+		}
+
+	}
+
+	public void preOrderTraversal(Node node) {
+		if (node != null) {
+			System.out.print(node.data + "-->");
+			preOrderTraversal(node.left);
+			preOrderTraversal(node.right);
+		}
+
+	}
+
+	public void postorderTraversal(Node node) {
+		if (node != null) {
+			postorderTraversal(node.left);
+			postorderTraversal(node.right);
+			System.out.print(node.data + "-->");
+		}
+	}
+}
---
+++ src/learningLogic/Tree/BinaryTreeRevisionMain.java
@@ -0,0 +1,21 @@
+package learningLogic.Tree;
+
+public class BinaryTreeRevisionMain {
+
+	public static void main(String[] args) {
+
+		BinaryTreeRevision bt = new BinaryTreeRevision();
+		bt.insert(10);
+		bt.insert(20);
+		bt.insert(30);
+		bt.insert(40);
+		bt.insert(50);
+		System.out.println("In order traversal data is : ");
+		bt.inOrderTraversal(bt.root);
+		System.out.println("\nPre order traversal data is : ");
+		bt.preOrderTraversal(bt.root);
+		System.out.println("\nPost order traversal data is : ");
+		bt.postorderTraversal(bt.root);
+	}
+
+}
