fa45abf408d96792e8ca94661dff8bf7a9b602b2
Nikhil <Nikhil.Thulaseedharan@gmail.com>
2022-10-01 21:34:38 +0530

    Binary Trees Using Linked List Revision

---
+++ src/revision/Tree/Node.java
@@ -0,0 +1,17 @@
+package revision.Tree;
+
+public class Node {
+
+	public int data;
+	public Node left;
+	public Node right;
+
+	public Node(int data) {
+		this.left = null;
+		this.right = null;
+		this.data = data;
+
+
+	}
+
+}
