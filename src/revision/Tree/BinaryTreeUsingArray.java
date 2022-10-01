4a050cab2c6da52dfd0f03c92287d9c8ff91d74b
Nikhil <Nikhil.Thulaseedharan@gmail.com>
2022-10-01 23:50:17 +0530

    Revision Binart Tree with Array

---
+++ src/revision/Tree/BinaryTreeUsingArray.java
@@ -0,0 +1,90 @@
+package revision.Tree;
+
+public class BinaryTreeUsingArray {
+	
+	int[] arr;
+	int lastUsedIndex = 0;
+
+	public BinaryTreeUsingArray(int size) {
+		arr = new int[size + 1];
+	}
+
+	public void insert(int value) {
+
+		if (isTreeFull()) {
+			System.out.println("Tree is full so cannot insert an element");
+			return;
+		}
+		arr[++lastUsedIndex] = value;
+
+	}
+
+	private boolean isTreeFull() {
+		if (lastUsedIndex == arr.length)
+			return true;
+		return false;
+	}
+
+	public void levelOrder() {
+		for (int i = 1; i <= lastUsedIndex; i++)
+			System.out.print(arr[i] + "-->");
+
+	}
+
+	public void preOrder(int root) {
+		if (root <= lastUsedIndex) {
+			System.out.print(arr[root] + "-->");
+			preOrder(root * 2);
+			preOrder(root * 2 + 1);
+		}
+
+	}
+
+	public void postOrder(int root) {
+		if (root <= lastUsedIndex) {
+
+			postOrder(root * 2);
+			postOrder(root * 2 + 1);
+			System.out.print(arr[root] + "-->");
+		}
+
+	}
+
+	public void inOrder(int root) {
+		if (root <= lastUsedIndex) {
+
+			inOrder(root * 2);
+			System.out.print(arr[root] + "-->");
+			inOrder(root * 2 + 1);
+
+		}
+
+	}
+
+	public void search(int value) {
+		for (int i = 1; i <= lastUsedIndex; i++) {
+			if (arr[i] == value) {
+				System.out.println("Value found at pos: " + i);
+				return;
+			}
+		}
+
+	}
+
+	public void delete(int value) {
+		for (int i = 1; i <= lastUsedIndex; i++) {
+			if (arr[i] == value) {
+				System.out.println("Replacing " + value + " with " + arr[lastUsedIndex]);
+				arr[i] = arr[lastUsedIndex];
+				lastUsedIndex--;
+				return;
+			}
+		}
+
+	}
+
+	public void deleteTree() {
+		arr = null;
+
+	}
+}
--- src/revision/Tree/BinaryTreeUsingLinkedList.java
+++ src/revision/Tree/BinaryTreeUsingLinkedList.java
@@ -1,31 +1,213 @@
 package revision.Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+
 public class BinaryTreeUsingLinkedList {
 
-	public static void main(String[] args) {
-		BinaryTree bt = new BinaryTree();
-		bt.insert(10);
-		bt.insert(20);
-		bt.insert(30);
-		bt.insert(40);
-		bt.insert(50);
-		System.out.println("In order traversal data is : ");
-		bt.inOrderTraversal(bt.root);
-		System.out.println("\nPre order traversal data is : ");
-		bt.preOrderTraversal(bt.root);
-		System.out.println("\nPost order traversal data is : ");
-		bt.postorderTraversal(bt.root);
-		System.out.println("\nLevel order traversal data is : ");
-		bt.levelOrderTraversal(bt.root);
-		System.out.println("\nSearching value in Tree:");
-		System.out.println("\nUsing inorder traversal");
-		bt.inDepthFirstSearch(bt.root, 50);
-		System.out.println("\nUsing Level order traversal");
-		bt.breadthFirstSearch(bt.root, 50);
-		System.out.println("Deleting a value from bianry tree: ");
-		bt.deleteFromBinaryTree(bt.root, 20);
-		System.out.println("\nLevel order traversal data after replacement with deepest node is : ");
-		bt.levelOrderTraversal(bt.root);
+	Node root;
+
+	public void insert(int data) {
+		Node node = new Node(data);
+		if (root == null) {
+			root = node;
+			return;
+		}
+
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(root);
+		while (!queue.isEmpty()) {
+			Node temp = queue.peek();
+			queue.remove();
+			
+			if (temp.left == null) {
+				temp.left = node;
+				return;
+			} else {
+				queue.add(temp.left);
+			}
+			
+			if(temp.right==null) {
+				temp.right = node;
+				return;
+			}else {
+				queue.add(temp.right);
+			}
+		}
 	}
+
+	public void inOrderTraversal(Node root) {
+		if (root == null)
+			return;
+		else {
+			inOrderTraversal(root.left);
+			System.out.print(root.data + "-->");
+			inOrderTraversal(root.right);
+		}
+
+	}
+
+	public void preOrderTraversal(Node root) {
+		if (root == null)
+			return;
+		else {
+			System.out.print(root.data + "-->");
+			preOrderTraversal(root.left);
+			preOrderTraversal(root.right);
+		}
+
+	}
+
+	public void postorderTraversal(Node root) {
+		if (root == null)
+			return;
+		else {
+			postorderTraversal(root.left);
+			postorderTraversal(root.right);
+			System.out.print(root.data + "-->");
+		}
+
+	}
+
+	public void levelOrderTraversal(Node root) {
+		if (root == null) {
+			return;
+		} else {
+			Queue<Node> queue = new LinkedList<Node>();
+			queue.add(root);
+			while (!queue.isEmpty()) {
+				Node temp = queue.peek();
+				queue.remove();
+				System.out.print(temp.data + "-->");
+				if (temp.left != null)
+					queue.add(temp.left);
+				if (temp.right != null)
+					queue.add(temp.right);
+
+			}
+		}
+
+	}
+
+	private int pos = 0;
+	public void inDepthFirstSearch(Node root, int data) {
+		if (root == null) {
+			return;
+		} else {
+			pos++;
+
+			if (root.data == data) {
+				System.out.println("value is found at: " + pos);
+				return;
+			}
+			inDepthFirstSearch(root.left, data);
+			inDepthFirstSearch(root.right, data);
+		}
+
+	}
+
+	public void breadthFirstSearch(Node root, int data) {
+		if (root == null) {
+			return;
+		} else {
+			Queue<Node> queue = new LinkedList<Node>();
+			queue.add(root);
+			int loc=0;
+			while (!queue.isEmpty()) {
+				loc++;
+				Node temp = queue.peek();
+				queue.remove();
+				if(temp.data==data) {
+					System.out.println("value is found at: " + loc);
+					return;
+				}
+				if (temp.left != null)
+					queue.add(temp.left);
+				if (temp.right != null)
+					queue.add(temp.right);
+			}
+		}
+
+	}
+
+	public void deleteFromBinaryTree(Node root, int value) {
+
+		if (root == null)
+			return;
+		else {
+			Queue<Node> queue = new LinkedList<Node>();
+			queue.add(root);
+			while (!queue.isEmpty()) {
+
+				Node temp = queue.peek();
+				queue.remove();
+				if (temp.data == value) {
+					Node deep = deepestNode();
+					System.out.println();
+					System.out.println("Deepest node is: " + deep.data);
+					temp.data = deep.data;
+					deleteDeepestNode(root);
+
+				}
+				if (temp.left != null)
+					queue.add(temp.left);
+				if (temp.right != null)
+					queue.add(temp.right);
+			}
+		}
+
+	}
+
+	private void deleteDeepestNode(Node root) {
+		if(root==null) 
+			return;
+		else {
+			Queue<Node> queue=new LinkedList<Node>();
+			queue.add(root);
+			Node previousNode=null;
+			Node currentNode=null;
+			while (!queue.isEmpty()) {
+				previousNode = currentNode;
+				currentNode = queue.peek();
+				queue.remove();
+				if(currentNode.left==null) {
+					previousNode.right=null;
+							return;
+				}
+				if (currentNode.right == null) {
+					currentNode.left=null;
+					return;
+				}
+
+				queue.add(currentNode.left);
+				queue.add(currentNode.right);
+			}
+		}
+		
+
+	}
+
+	private Node deepestNode() {
+		Node temp=null;
+		if (root == null) {
+			return null;
+		} else {
+			Queue<Node> queue = new LinkedList<Node>();
+			queue.add(root);
+			while (!queue.isEmpty()) {
+				temp = queue.peek();
+				queue.remove();
+				System.out.print(temp.data + "-->");
+				if (temp.left != null)
+					queue.add(temp.left);
+				if (temp.right != null)
+					queue.add(temp.right);
+
+			}
+		}
+		return temp;
+	}
+
 
 }
