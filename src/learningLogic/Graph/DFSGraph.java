58a5495ba9bfcce25a2fb90ca6b824fa2468eafa
Nikhil <Nikhil.Thulaseedharan@gmail.com>
2022-12-15 21:01:05 +0530

    DFS Using stack

--- src/learningLogic/Graph/DFSGraph.java
+++ src/learningLogic/Graph/DFSGraph.java
@@ -2,6 +2,7 @@ package learningLogic.Graph;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class DFSGraph {
 
@@ -27,19 +28,75 @@ public class DFSGraph {
 		}
 
 		System.out.println("Disconnected Graph: ");
-		ArrayList<Integer> responseBfsDisconnectedGraph = dfsDisconnectedGraph(arr, n, m);
-		for (Integer num : responseBfsDisconnectedGraph) {
+		ArrayList<Integer> responseDfsDisconnectedGraph = dfsDisconnectedGraph(arr, n, m);
+		for (Integer num : responseDfsDisconnectedGraph) {
 			System.out.println(num + " ");
 		}
 
-//		System.out.println("Connected Graph: ");
-//		ArrayList<Integer> responseBfsConnectedGraph = bfsConnectedGraph(arr, n, m);
-//		for (Integer num : responseBfsConnectedGraph) {
-//			System.out.println(num + " ");
-//		}
+		System.out.println("Connected Graph: ");
+		ArrayList<Integer> responseDfsConnectedGraph = dfsConnectedGraph(arr, n, m);
+		for (Integer num : responseDfsConnectedGraph) {
+			System.out.println(num + " ");
+		}
+
+		System.out.println("Disconnected Graph Using Stack: ");
+		ArrayList<Integer> responseBfsDisconnectedGraphUsingStack = dfsDisconnectedGraphUsingStack(arr, n, m);
+		for (Integer num : responseBfsDisconnectedGraphUsingStack) {
+			System.out.println(num + " ");
+		}
 
 		printGraph(arr, n);
 		scan.close();
+	}
+
+	private ArrayList<Integer> dfsDisconnectedGraphUsingStack(ArrayList<ArrayList<Integer>> arr, int n, int m) {
+		ArrayList<Integer> dfs = new ArrayList<Integer>();
+		Stack<Integer> st = new Stack<>();
+		boolean[] visited = new boolean[n + 1];
+		for (int i = 1; i <= n; i++) {
+			if (visited[i] == false) {
+				visited[i] = true;
+				st.push(i);
+				while (!st.isEmpty()) {
+
+					Integer num = st.pop();
+					dfs.add(num);
+					for (Integer vertices : arr.get(num)) {
+						if(visited[vertices]==false) {
+							visited[vertices]=true;
+							st.push(vertices);
+							
+						}
+					}
+				}
+
+
+			}
+		}
+		return dfs;
+	}
+
+	private ArrayList<Integer> dfsConnectedGraph(ArrayList<ArrayList<Integer>> arr, int n, int m) {
+		ArrayList<Integer> dfs = new ArrayList<Integer>();
+		boolean[] visited = new boolean[n + 1];
+		// for (int i = 1; i <= n; i++) {
+		if (visited[1] == false) {
+			dfsConnectedGraphRecursive(dfs, visited, 1, arr);
+			}
+		// }
+		return dfs;
+	}
+
+	private void dfsConnectedGraphRecursive(ArrayList<Integer> dfs, boolean[] visited, int i,
+			ArrayList<ArrayList<Integer>> arr) {
+		visited[i]=true;
+		dfs.add(i);
+		for (Integer vertices : arr.get(i)) {
+			if (visited[vertices] == false) {
+				dfsConnectedGraphRecursive(dfs, visited, vertices, arr);
+			}
+		}
+
 	}
 
 	private ArrayList<Integer> dfsDisconnectedGraph(ArrayList<ArrayList<Integer>> arr, int n, int m) {
