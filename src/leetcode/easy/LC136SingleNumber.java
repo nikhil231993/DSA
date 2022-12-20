b4fbc5968414c8eecbc82675c3ee7193fb61328e
Nikhil <Nikhil.Thulaseedharan@gmail.com>
2022-12-21 01:39:36 +0530

    Single Number Using Map

--- src/leetcode/easy/LC136SingleNumber.java
+++ src/leetcode/easy/LC136SingleNumber.java
@@ -1,11 +1,32 @@
 package leetcode.easy;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class LC136SingleNumber {
 
 	public static void main(String[] args) {
 		int[] arr = new int[] { 4, 1, 2, 1, 2 };
 
 		System.out.println(singleNumber(arr));
+		System.out.println(singleNumberUsingMap(arr));
+	}
+
+	private static Integer singleNumberUsingMap(int[] arr) {
+		HashMap<Integer, Integer> map = new LinkedHashMap<Integer, Integer>();
+		for (Integer num : arr) {
+			if (map.containsKey(num))
+				map.put(num, map.get(num) + 1);
+			else
+				map.put(num, 1);
+		}
+		
+		for(Map.Entry<Integer, Integer> m: map.entrySet())
+			if (m.getValue() == 1)
+				return m.getKey();
+
+		return -1;
 	}
 
 	private static int singleNumber(int[] arr) {
