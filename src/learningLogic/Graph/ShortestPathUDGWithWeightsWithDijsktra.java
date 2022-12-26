33297e319970fe5166d4788db19631f99fada151
Nikhil <Nikhil.Thulaseedharan@gmail.com>
2022-12-26 20:12:13 +0530

    Dijsktra Algo

---
+++ src/learningLogic/Graph/ShortestPathUDGWithWeightsWithDijsktra.java
@@ -0,0 +1,107 @@
+package learningLogic.Graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class NodeVertex implements Comparator<NodeVertex> {
+	int v;
+	int w;
+
+	public NodeVertex(int vertex, int weight) {
+		this.v = vertex;
+		this.w = weight;
+	}
+
+	public NodeVertex() {
+
+	}
+
+	@Override
+	public int compare(NodeVertex o1, NodeVertex o2) {
+		if (o1.w < o2.w) {
+			return -1;
+		}
+		if (o1.w > o2.w) {
+			return 1;
+		}
+		return 0;
+	}
+
+}
+public class ShortestPathUDGWithWeightsWithDijsktra {
+
+	public static void main(String[] args) {
+		createGraph();
+
+	}
+
+	private static void createGraph() {
+		Scanner scan = new Scanner(System.in);
+		ArrayList<ArrayList<NodeVertex>> adjList = new ArrayList<ArrayList<NodeVertex>>();
+		System.out.println("Enter the no of vertices: ");
+		int n = scan.nextInt();
+		System.out.println("Enter the no of edges: ");
+		int m = scan.nextInt();
+		System.out.println("Creating ArrayLIst of ArrayList: ");
+		for (int i = 0; i <= n; i++)
+			adjList.add(new ArrayList<>());
+		System.out.println("Enter the combination of edges pair: ");
+		for (int i = 1; i <= m; i++) {
+			System.out.println("Enter the " + i + " edge: ");
+			int u = scan.nextInt();
+			int v = scan.nextInt();
+			System.out.println("Enter the weight of the edge: ");
+			int w = scan.nextInt();
+			adjList.get(u).add(new NodeVertex(v, w));
+			adjList.get(v).add(new NodeVertex(u, w));
+		}
+
+		System.out.println("Enter the source : ");
+		int src = scan.nextInt();
+
+		System.out.println("Enter the destination : ");
+		int dest = scan.nextInt();
+
+		System.out.println("BFS Traversal of DisConnected Nodes Graph: ");
+
+		System.out.println("Shortes Path : ");
+		shortestPath(adjList, n, src, dest);
+
+		scan.close();
+
+	}
+
+	private static void shortestPath(ArrayList<ArrayList<NodeVertex>> adjList, int n, int src, int dest) {
+
+		int[] path = new int[n + 1];
+		Arrays.fill(path, Integer.MAX_VALUE);
+
+		PriorityQueue<NodeVertex> pq = new PriorityQueue<NodeVertex>(n, new NodeVertex());
+		pq.add(new NodeVertex(src, 0));
+		
+		path[src] = 0;
+
+		while (!pq.isEmpty()) {
+			NodeVertex nodeVertex = pq.poll();
+
+			for (NodeVertex vertex : adjList.get(nodeVertex.v)) {
+				if (path[nodeVertex.v] + vertex.w < path[vertex.v]) {
+					System.out.println("Hi");
+					path[vertex.v] = path[nodeVertex.v] + vertex.w;
+					pq.add(new NodeVertex(vertex.v, path[vertex.v]));
+				}
+			}
+		}
+		int i = 0;
+		for (int num : path) {
+			System.out.println(i + "-->" + num + " ");
+			if (dest == i)
+				System.out.println("Shortest distance from src : + " + src + " to dest : " + dest + " is : " + num);
+			i++;
+		}
+	}
+
+}
