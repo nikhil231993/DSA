3809486cd4251065eca7560d38789833c3240aea
Nikhil <Nikhil.Thulaseedharan@gmail.com>
2023-03-14 23:20:35 +0530

    LC167

---
+++ src/leetcode/medium/array/LC167TwoSum2SortedArray.java
@@ -0,0 +1,33 @@
+package leetcode.medium.array;
+
+public class LC167TwoSum2SortedArray {
+
+	public static void main(String[] args) {
+		int[] nums = new int[] { 2, 3, 4 };
+		int target = 6;
+
+		int[] arr = sum(nums, target);
+		for (int n : arr)
+			System.out.println(n);
+
+	}
+
+	private static int[] sum(int[] nums, int target) {
+		int i=0;
+		int j = nums.length - 1;
+		
+		while(i<j) {
+			if(nums[i]+nums[j]==target)
+				return new int[] { i + 1, j + 1 };
+			else if (nums[i] + nums[j] > target)
+				j--;
+			else
+				i++;
+			// SC:O(1)
+			// TC:O(n)
+
+		}
+		return new int[] {};
+	}
+
+}
