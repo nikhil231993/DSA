ea11043c35762b8f587eda256af4009d133f6b36
Nikhil Thulaseedharan <nikhilthulaseedharan@Nikhils-MacBook-Air.local>
2023-04-08 10:49:13 +0530

    Fixing some errors

--- src/leetcode/array/easy/LC169MajorityElement.java
+++ src/leetcode/array/easy/LC169MajorityElement.java
@@ -1,5 +1,8 @@
 package leetcode.array.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LC169MajorityElement {
 
 	public static void main(String[] args) {
@@ -7,6 +10,38 @@ public class LC169MajorityElement {
 		System.out.println(majorityElement(nums));
 
 	}
+	//Brute force
+
+	public int majorityElementBruteForce(int[] nums) {
+		// int max=Integer.MIN_VALUE;
+		// Arrays.sort(nums);
+		// return nums[nums.length/2]; After sorting sicne element is majority in array it will be present at middle position
+		//TC:O(1)
+		//SC:O(1)
+
+		int size=nums.length/2;
+		HashMap<Integer,Integer> map=new HashMap<>();
+		for(int i=0;i<nums.length;i++){
+			if(map.containsKey(nums[i])){
+				map.put(nums[i],map.get(nums[i])+1);
+			}else{
+				map.put(nums[i],1);
+			}
+		}
+
+		int val=0;
+		int no=0;
+		for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+			if(entry.getValue()>=size+1){
+				val=entry.getValue();
+				no=entry.getKey();
+			}
+		}
+		return no;
+
+	}
+
+	//Best Approach
 
 	private static int majorityElement(int[] nums) {
 		int count = 0;
