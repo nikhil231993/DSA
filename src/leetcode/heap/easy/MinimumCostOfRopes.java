5b7e4bb2672a786adaffb692e162cc01d9a1a6c9
nikhil231993 <nikhil.thulaseedharan@gmail.com>
2023-08-23 23:39:36 +0530

    Minimum cost of ropes

---
+++ src/leetcode/heap/easy/MinimumCostOfRopes.java
@@ -0,0 +1,31 @@
+package leetcode.heap.easy;
+
+import java.util.PriorityQueue;
+
+public class MinimumCostOfRopes {
+    public static void main(String[] args) {
+
+        int[] arr=new int[]{4,3,2,6};
+        int n=4;
+        System.out.println(minCost(arr,n));
+}
+
+    public static int minCost(int arr[], int n)
+    {
+        PriorityQueue<Integer> pq=new PriorityQueue();
+        for(Integer num:arr)
+            pq.offer(num);
+
+        Integer cost=0;
+        while(pq.size()!=1){
+            Integer a=pq.poll();
+            Integer b=pq.poll();
+            cost+=a+b;
+
+            pq.offer(a+b);
+        }
+        return cost;
+        //TC:log n
+        //SC:n
+    }
+}
