8bf3190b1f870ec41c263f541a7f6f0062cf3c6d
nikhil231993 <nikhil.thulaseedharan@gmail.com>
2023-08-23 23:08:02 +0530

    LC347

---
+++ src/leetcode/heap/medium/LC347TopKFrequentElements.java
@@ -0,0 +1,36 @@
+package leetcode.heap.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+class Node{
+    int value;
+    int key;
+    public Node(int value, int key){
+        this.value=value;
+        this.key=key;
+    }
+}
+public class LC347TopKFrequentElements {
+    public static void main(String[] args) {
+        int[] nums=new int[]{1};
+        int k=1;
+
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int n:nums){
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+        PriorityQueue<Node> pq=new PriorityQueue<Node>((a,b)->a.value-b.value);
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){//O(N)
+            Node n=new Node(entry.getValue(), entry.getKey());
+            pq.offer(n);//log k
+            if(pq.size()>k)
+                pq.poll();//log k
+        }
+        while(!pq.isEmpty()){
+            System.out.println(pq.poll().key);
+        }
+        //TC: O(NlogK)
+    }
+}
