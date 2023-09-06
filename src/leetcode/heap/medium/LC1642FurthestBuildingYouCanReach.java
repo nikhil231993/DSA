e291286f91b683b398f80afb8f2677ded2425ed9
nikhil231993 <nikhil.thulaseedharan@gmail.com>
2023-09-06 21:21:22 +0530

    Binary Search 1

---
+++ src/leetcode/heap/medium/LC1642FurthestBuildingYouCanReach.java
@@ -0,0 +1,33 @@
+package leetcode.heap.medium;
+
+import java.util.PriorityQueue;
+
+public class LC1642FurthestBuildingYouCanReach {
+    public static void main(String[] args) {
+        int[] heights = new int[]{4,12,2,7,3,18,20,3,19};
+        int bricks = 10, ladders = 2;
+
+        System.out.println(furthestBuilding(heights,bricks,ladders));
+
+    }
+    public static int furthestBuilding(int[] heights, int bricks, int ladders) {
+
+        PriorityQueue<Integer> pq=new PriorityQueue();
+        for(int i=0;i< heights.length-1;i++){
+            int gap=heights[i+1]-heights[i];
+            if(gap>0)
+                pq.offer(gap);
+            if(pq.size()>ladders){
+                bricks-=pq.poll();
+            }
+
+            if(bricks<0)
+                return i;
+
+        }
+        return heights.length-1;
+        //TC:(ladders log ladders
+        //SC:O(ladders)
+
+    }
+}
---
+++ src/leetcode/heap/medium/LC378KthSmallestInASortedMatrix.java
@@ -0,0 +1,65 @@
+package leetcode.heap.medium;
+
+import java.util.PriorityQueue;
+public class LC378KthSmallestInASortedMatrix {
+    public static void main(String[] args) {
+        int[][] matrix =new int[][] {{1,5,9},{10,11,13},{12,13,15}};
+        int k = 8;
+        //Approach 1:
+        System.out.println(kthSmallest(matrix,k));
+
+        //Approach 2:
+
+        System.out.println(KthSmallestUsingBinarySearch(matrix,k));
+    }
+
+    private static int KthSmallestUsingBinarySearch(int[][] matrix,int k) {
+        int size=matrix.length;
+        int lowest=matrix[0][0];
+        int highest=matrix[size-1][size-1];
+
+        while(lowest<highest){
+            int mid=lowest+(highest-lowest)/2;
+            int count= countLessOrEqual(mid,matrix,k);
+            if(count<k)
+                lowest=mid+1;
+            else
+                highest=mid;
+        }
+        return lowest;
+    }
+
+    private static int countLessOrEqual(int mid, int[][] matrix, int k) {
+        int i=matrix.length-1;
+        int j=0;
+        int count=0;
+        while(i>=0 && j<matrix.length){
+            if(matrix[i][j]>mid)
+                i--;
+            else{
+                count=count+i+1;
+                j++;
+            }
+        }
+        return count;
+        //TC:O(n*n*log n)
+        //SC:O(1)
+    }
+
+    public static int kthSmallest(int[][] matrix, int k) {
+
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a, b)-> b - a);
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                pq.offer(matrix[i][j]);
+                if(pq.size()>k)
+                    pq.poll();
+            }
+        }
+        return pq.peek();
+        //TC:O(n square log k)
+        //SC:O(k)
+
+    }
+}
