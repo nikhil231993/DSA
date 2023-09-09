138da9a573b813f2c148cdc841f1c7ca681dae72
nikhil231993 <nikhil.thulaseedharan@gmail.com>
2023-09-09 11:12:29 +0530

    BS 10

---
+++ src/leetcode/binarysearch/BS10SquareRootLC69.java
@@ -0,0 +1,29 @@
+package leetcode.binarysearch;
+
+public class BS10SquareRootLC69 {
+
+    public static void main(String[] args) {
+        int x=28;
+        System.out.println(mySqrt(x));
+    }
+
+    public static int mySqrt(int x) {
+
+        int low=1;
+        int high=x;
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(x/mid<mid){
+                high=mid-1;
+            }else{
+                ans=mid;
+                low=mid+1;
+            }
+        }
+        return ans;
+        //TC:log n
+        //SC: 1
+    }
+}
--- src/leetcode/binarysearch/BS9FindPeakElement.java
+++ src/leetcode/binarysearch/BS9FindPeakElement.java
@@ -25,12 +25,14 @@ public class BS9FindPeakElement {
 
             if(nums[mid]> nums[mid-1] && nums[mid]>nums[mid+1])
                 return mid;
-            if(nums[mid]<nums[mid+1])
+            if(nums[mid]>nums[mid-1])
                 low=mid+1;
             else
                 high=mid-1;
         }
 
         return -1;
+        //TC:O(log n)
+        //SC:O(n )
     }
 }
