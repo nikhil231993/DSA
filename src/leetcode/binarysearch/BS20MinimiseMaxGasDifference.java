eaa1e2288811f02234c1336631b1a524ee247016
nikhil231993 <nikhil.thulaseedharan@gmail.com>
2023-09-12 23:19:56 +0530

    BS21

---
+++ src/leetcode/binarysearch/BS20MinimiseMaxGasDifference.java
@@ -0,0 +1,120 @@
+package leetcode.binarysearch;
+
+import java.util.PriorityQueue;
+class Node{
+
+    double first;
+    int second;
+
+    public Node(double first, int second){
+        this.first=first;
+        this.second=second;
+    }
+}
+public class BS20MinimiseMaxGasDifference {
+
+    public static void main(String[] args) {
+        int n=5,k=6;
+        int[] arr=new int[]{1,2,3,4,5,6,7};
+
+        //Approach 1
+        System.out.println(minimiseMaxDistance(arr,k));
+
+        //Approach 2
+        System.out.println(minimiseMaxDistancePriorityQueue(arr,k));
+
+        //Approach 3
+        System.out.println(minimiseMaxDistanceUsingBinarySearch(arr,k));
+    }
+
+    private static double minimiseMaxDistancePriorityQueue(int[] arr, int k) {
+        int[] howMany=new int[arr.length-1];
+        PriorityQueue<Node> pq=new PriorityQueue<>((a, b)->Double.compare(b.first, a.first));
+
+        for(int i=0;i< arr.length-1;i++){
+            pq.offer(new Node(arr[i+1]-arr[i], i));
+        }
+        for(int gas=1;gas<=k;gas++){
+            Node n=pq.poll();
+            int index=n.second;
+            howMany[index]++;
+            double newSectionLength=(double)(arr[index+1]-arr[index])/(double)(howMany[index]+1);
+            pq.offer(new Node(newSectionLength,index));
+        }
+        return pq.peek().first;
+        //TC:O(n logn)+O(k log (n-1))
+        //SC:O(n-1)
+    }
+
+    public static double minimiseMaxDistance(int []arr, int k){
+        int[] howMany=new int[arr.length-1];
+        for(int gas=1;gas<=k;gas++){
+            double maxDiff=Integer.MIN_VALUE;
+            int maxIndex=Integer.MIN_VALUE;
+
+            for(int i=0;i<arr.length-1;i++){
+                double difference=arr[i+1]-arr[i];
+                double sectionLength=difference/howMany[i];
+                if(sectionLength> maxDiff){
+                    maxDiff=sectionLength;
+                    maxIndex=i;
+                }
+            }
+
+            howMany[maxIndex]++;
+        }
+        double max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length-1;i++){
+            double diff=arr[i+1]-arr[i];
+            double ans=diff/(double)(howMany[i]+1);
+            max=Math.max(max, ans);
+        }
+        return max;
+        //TC:O(k * n)+O(k)
+        //SC:O(1)
+    }
+
+    public static double minimiseMaxDistanceUsingBinarySearch(int []arr, int K){
+        double low=0;
+        double high=maxValue(arr);
+        double ans=0;
+        double diff=1e-6;
+        while(high-low > diff){
+            double mid=low+(high-low)/2.0;
+            int count=isPossible(mid, arr, K);
+            if(count>K){
+                low=mid;
+            }else{
+                ans=mid;
+                high=mid;
+            }
+        }
+        return ans;
+
+        //TC:O( nlog n)+O(n)
+        //SC:O(1)
+
+    }
+
+    public static int isPossible(double mid, int[] arr, int K){
+        int count=0;
+        for(int i=0;i<arr.length-1;i++){
+            int no=(int)((arr[i+1]-arr[i])/mid);
+            if((arr[i+1]-arr[i]) == no*mid){
+                no--;
+            }
+            count+=no;
+        }
+        return count;
+    }
+
+    public static double maxValue(int[] arr){
+
+        double max=0.0;
+        for(int i=0;i<arr.length-1;i++){
+            max=Math.max(max,(double)(arr[i+1]-arr[i]));
+        }
+        return max;
+    }
+
+}
---
+++ src/leetcode/binarysearch/BS21MedianTwoSortedArray.java
@@ -0,0 +1,124 @@
+package leetcode.binarysearch;
+
+public class BS21MedianTwoSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = new int[] {1,2};
+        int[] nums2 = new int[] {3,4};
+
+        //Brute force
+        System.out.println(findMedianSortedArrays(nums1,nums2));
+        //Better
+        System.out.println(findMedianSortedArraysWithoutSpace(nums1,nums2));
+        //Optimal
+        System.out.println(findMedianSortedArraysBS(nums1,nums2));
+    }
+
+    public static  double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int[] nums3=new int[n1+n2];
+        int n=n1+n2;
+
+        int i=0,j=0;
+        int k=0;
+        while(i<n1 && j< n2){
+            if(nums1[i] < nums2[j]){
+                nums3[k++]=nums1[i++];
+            }else{
+                nums3[k++]=nums2[j++];
+            }
+        }
+        while(i<n1){
+            nums3[k++]=nums1[i++];
+        }
+        while(j<n2){
+            nums3[k++]=nums2[j++];
+        }
+        if(n%2==1)
+            return nums3[n/2];
+        else
+            return (double)((double)(nums3[n/2]+nums3[n/2-1])/(double)2.0);
+        //TC:O(n+m)
+        //SC:O(n+m)
+    }
+
+    public static double findMedianSortedArraysWithoutSpace(int[] nums1, int[] nums2) {
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int n=n1+n2;
+        int count=0;
+        int index2=n/2;
+        int index1=index2-1;
+        int i=0,j=0;
+        int element1=-1;
+        int element2=-1;
+        while(i<n1 && j< n2){
+            if(nums1[i] < nums2[j]){
+                if(count==index1) element1=nums1[i];
+                if(count==index2) element2=nums1[i];
+                count++;
+                i++;
+            }else{
+                if(count==index1) element1=nums2[j];
+                if(count==index2) element2=nums2[j];
+                count++;
+                j++;
+            }
+        }
+        while(i<n1){
+            if(count==index1) element1=nums1[i];
+            if(count==index2) element2=nums1[i];
+            count++;
+            i++;
+        }
+        while(j<n2){
+            if(count==index1) element1=nums2[j];
+            if(count==index2) element2=nums2[j];
+            count++;
+            j++;
+        }
+        if(n%2==1)
+            return element2;
+        else
+            return (double)(element1+element2)/2.0;
+        //TC:O(n+m)
+        //SC:O(n+m)
+    }
+
+    public static double findMedianSortedArraysBS(int[] nums1, int[] nums2) {
+
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int n=n1+n2;
+
+        if(n1>n2)
+            return findMedianSortedArrays(nums2,nums1);
+        int low=0;
+        int high=n1;
+        int left=(n1+n2+1)/2;
+        while(low<=high){
+            int mid1=low+(high-low)/2;
+            int mid2=left-mid1;
+            int l1=Integer.MIN_VALUE;
+            int l2=Integer.MIN_VALUE;
+            int r1=Integer.MAX_VALUE;
+            int r2=Integer.MAX_VALUE;
+            if(mid1<n1) r1=nums1[mid1];
+            if(mid2<n2) r2=nums2[mid2];
+            if(mid1-1>=0) l1=nums1[mid1-1];
+            if(mid2-1>=0) l2=nums2[mid2-1];
+            if(l1<=r2 && l2<=r1){
+                if(n%2 == 1)
+                    return Math.max(l1,l2);
+                else
+                    return (double)(Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+            }
+            else if(l1>r2)
+                high=mid1-1;
+            else low=mid1+1;
+        }
+        return 0.0;
+        //TC:log(size small array)
+
+    }
+}
