44e739d520b65d8d68b339820106e4115d4a8bea
nikhil231993 <nikhil.thulaseedharan@gmail.com>
2023-10-20 17:47:26 +0530

    LC506

---
+++ src/leetcode/heap/easy/LC506Rankings.java
@@ -0,0 +1,59 @@
+package leetcode.heap.easy;
+
+import java.util.PriorityQueue;
+
+class Node1{
+    int key;
+    int value;
+    public Node1(int key, int value){
+        this.key=key;
+        this.value=value;
+    }
+}
+public class LC506Rankings {
+
+    public static void main(String[] args) {
+        int[] score =new int[] {5,4,3,2,1};
+        String[] result=findRelativeRanks(score);
+        for(String s: result)
+            System.out.println(s);
+    }
+
+    public static String[] findRelativeRanks(int[] score) {
+        int n=score.length;
+        PriorityQueue<Node1> pq=new PriorityQueue<>((a, b)->b.key-a.key);
+        int i=0;
+        for(int n1:score)
+            pq.offer(new Node1(n1,i++));
+        String[] str=new String[score.length];
+        if(n>=1){
+            int key=pq.peek().key;
+            int index=pq.peek().value;
+            str[index]="Gold Medal";
+            pq.poll();
+        }
+        if(n>=2){
+
+            int key1=pq.peek().key;
+            int index1=pq.peek().value;
+            str[index1]="Silver Medal";
+            pq.poll();
+        }
+        if(n>=3){
+
+            int key2=pq.peek().key;
+            int index2=pq.peek().value;
+            str[index2]="Bronze Medal";
+            pq.poll();
+        }
+        if(n>=4){
+            int k=4;
+            while(!pq.isEmpty()){
+                int n1=pq.peek().value;
+                pq.poll();
+                str[n1]=String.valueOf(k++);
+            }
+        }
+        return str;
+    }
+}
