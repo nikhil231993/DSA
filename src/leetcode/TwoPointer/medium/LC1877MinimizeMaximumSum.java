ec7461f92507582fc55a78984ef78cb3ea6ce6ab
nikhil231993 <nikhil.thulaseedharan@gmail.com>
2023-12-04 18:50:28 +0530

    LC2942

---
+++ src/leetcode/TwoPointer/medium/LC1877MinimizeMaximumSum.java
@@ -0,0 +1,23 @@
+package leetcode.TwoPointer.medium;
+
+import java.util.Arrays;
+
+public class LC1877MinimizeMaximumSum {
+
+    public static void main(String[] args) {
+       int[] nums =new int[] {3,5,2,3};
+       System.out.println(minPairSum(nums));
+    }
+
+    public static int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int left=0;
+        int right=nums.length-1;
+        int max=Integer.MIN_VALUE;
+        while(left<right){
+            max=Math.max(max, nums[right--]+nums[left++]);
+        }
+        return max;
+    }
+
+}
---
+++ src/leetcode/array/easy/LC2942CharInWords.java
@@ -0,0 +1,22 @@
+package leetcode.array.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC2942CharInWords {
+
+    public static void main(String[] args) {
+        String[] words = new String[]{"leet","code"};
+        char x = 'e';
+        System.out.println(findWordsContaining(words,x));
+    }
+
+    public static List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> list=new ArrayList();
+        for(int i=0;i<words.length;i++){
+            if(words[i].indexOf(x)!=-1)
+                list.add(i);
+        }
+        return list;
+    }
+}
