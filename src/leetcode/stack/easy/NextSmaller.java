b094224cb3fc8b49f4622b1cbccd6f794498c068
Nikhil <Nikhil.Thulaseedharan@gmail.com>
2024-02-20 11:32:12 +0530

    Next and Previous Smaller

---
+++ src/leetcode/stack/easy/NextSmaller.java
@@ -0,0 +1,75 @@
+package leetcode.stack.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+public class NextSmaller {
+
+    public static void main(String[] args) {
+    	
+        int[] nums=new int[]{1,5,3,4,6,7,2,8,9};
+        
+		// Approach 1
+		findNextMinimumOfEachElement(nums, new Stack<>());
+        for(int i=0;i<nums.length;i++)
+            System.out.println(nums[i]);
+        
+		System.out.println("###################################");
+
+        ArrayList<Integer> arr=new ArrayList<>();
+        arr.add(2);
+        arr.add(1);
+        arr.add(4);
+        arr.add(3);
+        
+		// Approach 2
+		ArrayList<Integer> ans = nextSmallerElement(arr, arr.size());
+        		
+        for(Integer n: ans)
+        	System.out.println(n);
+        
+    }
+
+    private static void findNextMinimumOfEachElement(int[] nums, Stack<Integer> st) {
+
+        for(int i=0;i<nums.length;i++){
+            while(!st.isEmpty() && nums[st.peek()]>nums[i]){
+                int index=st.pop();
+                nums[index]=nums[i];
+            }
+            st.push(i);
+        }
+
+        while(!st.isEmpty()){
+            nums[st.pop()]=-1;
+        }
+    }
+
+	public static ArrayList<Integer> nextSmallerElement(ArrayList<Integer> arr, int n) {
+
+		Stack<Integer> st = new Stack();
+
+		int[] arr1 = new int[arr.size()];
+
+		for (int i = 0; i < arr.size(); i++) {
+
+			while (!st.isEmpty() && arr.get(st.peek()) > arr.get(i)) {
+				arr1[st.peek()] = arr.get(i);
+				st.pop();
+			}
+			st.push(i);
+		}
+
+		while (!st.isEmpty())
+			arr1[st.pop()] = -1;
+
+		ArrayList<Integer> result = (ArrayList<Integer>) Arrays.stream(arr1).boxed().collect(Collectors.toList());
+//		for (Integer n1 : arr1)
+//			result.add(n1);
+//      int[] example1 = list.stream().mapToInt(i->i).toArray();
+
+		return result;
+	}
+}
--- src/leetcode/stack/easy/PreviousSmaller.java
+++ src/leetcode/stack/easy/PreviousSmaller.java
@@ -1,15 +1,30 @@
 package leetcode.stack.easy;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class PreviousSmaller {
 
     public static void main(String[] args) {
+
         int[] nums=new int[]{1,5,3,4,6,7,2,8,9};
         Stack<Integer> st=new Stack<>();
+
+		// Approach 1
         findPrevMinimumOfEachElement(nums,st);
         for(int n: nums)
             System.out.println(n);
+
+		System.out.println("#######################################");
+		int[] nums1 = new int[] { 1, 5, 3, 4, 6, 7, 2, 8, 9 };
+
+		// Approach 2:
+		List<Integer> result = leftSmaller(nums1.length, nums1);
+		for (int no : result)
+			System.out.println(no);
+
     }
 
     private static void findPrevMinimumOfEachElement(int[] nums, Stack<Integer> st) {
@@ -26,4 +41,27 @@ public class PreviousSmaller {
             nums[st.pop()]=-1;
         }
     }
+
+	private static List<Integer> leftSmaller(int n, int a[]) {
+		Stack<Integer> st = new Stack();
+
+		int[] ans = new int[a.length];
+
+		for (int i = a.length - 1; i >= 0; i--) {
+
+			while (!st.isEmpty() && a[st.peek()] > a[i]) {
+
+				ans[st.peek()] = a[i];
+				st.pop();
+			}
+			st.push(i);
+		}
+
+		while (!st.isEmpty()) {
+			ans[st.pop()] = -1;
+		}
+
+		List<Integer> result = Arrays.stream(ans).boxed().collect(Collectors.toList());
+		return result;
+	}
 }
