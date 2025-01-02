36a51976e56d31e386b9df1d3b69581616d964f2
nikhil231993 <nikhil.thulaseedharan@gmail.com>
2025-01-02 23:14:55 +0530

    DP32

--- src/leetcode/dynamicprogramming/DP32LC115DistinctSubsequences.java
+++ src/leetcode/dynamicprogramming/DP32LC115DistinctSubsequences.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 public class DP32LC115DistinctSubsequences {
 
     public static void main(String[] args) {
-        String s = "rabbbit", t = "rabbit";
+
+        //String s = "rabbbit", t = "";
+        String s="banana",t="ban";
 
         int index1=s.length();
         int index2=t.length();
@@ -21,8 +23,6 @@ public class DP32LC115DistinctSubsequences {
 
         //Tabulation
         int[][] dp1=new int[index1+1][index2+1];
-        for(int[] r:dp1)
-            Arrays.fill(r,-1);
         System.out.println(tabulation(s,t,index1,index2,dp1));
 
         //space
@@ -33,6 +33,9 @@ public class DP32LC115DistinctSubsequences {
 
         int[] prev=new int[index2+1];
         prev[0]=1;
+
+        for(int j=1;j<=index2;j++) // we don't need this as it is already set to 0
+            prev[j]=0;
 
         for(int ind1=1;ind1<=index1;ind1++){
             int[] curr=new int[index2+1];
@@ -53,7 +56,8 @@ public class DP32LC115DistinctSubsequences {
 
         for(int i=0;i<=index1;i++)
             dp1[i][0]=1;
-        //Below we start from 1 because for all j=0 it should be 1. We can even omit it
+
+        //Below we start from 1 because for all j=0 it should be 1. We can even omit it as by default it is 0 same LCS where we dont have to initialize anything
         for(int j=1;j<=index2;j++)
             dp1[0][j]=0;
 
@@ -67,6 +71,7 @@ public class DP32LC115DistinctSubsequences {
             }
         }
         return dp1[index1][index2];
+
         //TC:O(index1 * index2)
         //SC:O(index1*index2)
     }
@@ -82,23 +87,23 @@ public class DP32LC115DistinctSubsequences {
 
         if(s.charAt(index1-1)==t.charAt(index2-1))
             return dp[index1][index2]=memoization(s,t,index1-1,index2-1,dp)+ memoization(s,t,index1-1,index2,dp);
-        else
-            return dp[index1][index2]=memoization(s,t,index1-1,index2,dp);
+        return dp[index1][index2]=memoization(s,t,index1-1,index2,dp);
+
         //TC:O(index1 * index2)
         //SC:O(index1+index2)+O(index1*index2)
     }
 
     private static int recursion(String s, String t, int index1, int index2) {
 
-        if(index2==0)
+        if(index2==0) // we ideally use <0 but similar to LCS we increase the index by 1
             return 1;
         if(index1==0)
             return 0;
 
         if(s.charAt(index1-1)==t.charAt(index2-1))
             return recursion(s,t,index1-1,index2-1)+ recursion(s,t,index1-1,index2);
-        else
-            return recursion(s,t,index1-1,index2);
+        return recursion(s,t,index1-1,index2);
+
         //TC:O(2 raise to index1 * 2 raise to index2)
         //SC:O(index1+index2)
     }
